d2173ac8e6702b0f67d9f984c64e61ca4a6e45e8
Jerin133 <jj0942754@gmail.com>
2026-04-14 16:14:05 +0530

    Update final_app_ESP32_code.ino
    It has the newly added features in it.....

--- final_app_ESP32_code/final_app_ESP32_code.ino
+++ final_app_ESP32_code/final_app_ESP32_code.ino
@@ -1,84 +1,98 @@
 #define BLYNK_TEMPLATE_ID "TMPL3TcenC7UX"
 #define BLYNK_TEMPLATE_NAME "food spoilage detection"
 
-#define BLYNK_PRINT Serial
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Initialize LCD (Address 0x27 is common, if not working try 0x3F)
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
 char auth[] = "7EVX6MLdiH3BbsCg8swIH4MebfiYLoWp";
-char ssid[] = "DINESH";
-char pass[] = "dinesht2x";
+char ssid[] = "Redmi 12 5G";
+char pass[] = "password";
 
 #define GAS_SENSOR 34
+#define FIRE_SENSOR 35 
 #define RED_LED 2
 #define GREEN_LED 15
 #define BUZZER 4
 
-int THRESHOLD = 2000;
+int GAS_THRESHOLD = 1300;
+int FIRE_THRESHOLD = 2000; 
 
 BlynkTimer timer;
+bool gasNotified = false;
+bool fireNotified = false;
 
-void sendSensorData() {
-  int value = analogRead(GAS_SENSOR);
+void checkSensors() {
+  int gasValue = analogRead(GAS_SENSOR);
+  int fireValue = analogRead(FIRE_SENSOR);
+  int fireIntensity = 4095 - fireValue; 
 
-  Serial.println(value);
+  bool isGasAlert = (gasValue > GAS_THRESHOLD);
+  bool isFireAlert = (fireIntensity > FIRE_THRESHOLD);
 
-  if (value > THRESHOLD) {
+  // --- LCD UPDATE LOGIC ---
+  lcd.clear();
+  if (isGasAlert || isFireAlert) {
     digitalWrite(RED_LED, HIGH);
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(BUZZER, HIGH);
-    Blynk.logEvent("food_spoiled", "Warning: Food Spoilage Detected!");
-    Blynk.virtualWrite(V12, "Food Spoiled 🚨");
-  } else {
+
+    lcd.setCursor(0, 0);
+    lcd.print("!! WARNING !!");
+    lcd.setCursor(0, 1);
+    
+    if (isFireAlert) {
+        lcd.print("FIRE DETECTED!");
+        if(!fireNotified) { Blynk.logEvent("fire_alert", "Fire detected in Fridge!"); fireNotified = true; }
+    } else {
+        lcd.print("FOOD SPOILED!");
+        if(!gasNotified) { Blynk.logEvent("food_spoiled", "Food Spoilage Detected in Fridge!"); gasNotified = true; }
+    }
+  } 
+  else {
     digitalWrite(RED_LED, LOW);
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(BUZZER, LOW);
-    Blynk.virtualWrite(V12, value);
+    gasNotified = false;
+    fireNotified = false;
+
+    // Normal Display
+    lcd.setCursor(0, 0);
+    lcd.print("Gas: "); lcd.print(gasValue);
+    lcd.setCursor(0, 1);
+    lcd.print("Fire: "); lcd.print(fireIntensity);
   }
+
+  // Update Blynk
+  Blynk.virtualWrite(V12, gasValue);
+  Blynk.virtualWrite(V13, fireIntensity);
 }
 
 void setup() {
   Serial.begin(115200);
-  delay(2000);
-  Serial.println("\n--- Food Spoilage System Booting ---");
+  
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("System Booting...");
 
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
 
-  // FORCE STATION MODE
-  WiFi.mode(WIFI_STA); 
   WiFi.begin(ssid, pass);
-
-  Serial.print("Connecting to WiFi: ");
-  Serial.println(ssid);
-
-  // We only wait 10 seconds here so the sensor can start working quickly
-  int attempt = 0;
-  while (WiFi.status() != WL_CONNECTED && attempt < 10) {
-    delay(1000);
-    Serial.print(".");
-    attempt++;
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nWiFi Connected! IP: " + WiFi.localIP().toString());
-    Blynk.config(auth);
-    Blynk.connect();
-  } else {
-    Serial.println("\nWiFi not ready yet. System starting in Offline Mode...");
-    // It will keep trying to connect in the background
-  }
-
-  timer.setInterval(1000L, sendSensorData);
+  Blynk.config(auth);
+  
+  timer.setInterval(1500L, checkSensors); // Increased slightly for LCD stability
 }
 
 void loop() {
-  // Only run Blynk if we actually have a connection
-  if (WiFi.status() == WL_CONNECTED) {
-    Blynk.run();
-  }
+  Blynk.run();
   timer.run();
 }
